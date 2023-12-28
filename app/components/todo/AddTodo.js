7c08f86af1e10a29b65594fce60433e4667fc60b
ManavYagnik <yagnik.c@ahduni.edu.in>
2023-12-29 05:28:48 +0530

    some changes

--- app/components/todo/AddTodo.js
+++ app/components/todo/AddTodo.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { db } from "../../firebase";
 import { collection, addDoc } from "firebase/firestore";
@@ -9,8 +10,8 @@ const { user } = UserAuth();
  
   const [title, setTitle] = React.useState("");
   const [date,setDate] =React.useState("");
-  console.log(user.email)
-  const c= user.email
+
+  const c= user.email;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
