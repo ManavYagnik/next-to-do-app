584f27790ccc7426d1884fe8e7b05716e3da62d2
ManavYagnik <yagnik.c@ahduni.edu.in>
2024-01-08 00:51:10 +0530

    removed some empty spaces

--- app/firebase.js
+++ app/firebase.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
-
 import {getFirestore} from "firebase/firestore";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBtNfC8C3IUjE_6jllNGLIQJIrQ5TT-eA8",
   authDomain: "to-do-12034.firebaseapp.com",
@@ -15,11 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "351143660964",
   appId: "1:351143660964:web:f35a0da6ed8795fb63c531"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
 const db = getFirestore(app);
-
 export {db};
--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,7 @@ import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { AuthContextProvider } from "./context/AuthContext";
-
 const inter = Inter({ subsets: ["latin"] });
-
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
