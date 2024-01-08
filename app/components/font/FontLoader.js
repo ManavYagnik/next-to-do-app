2951c06aeb151a80c319bb0eb392dca1698cf0f0
ManavYagnik <yagnik.c@ahduni.edu.in>
2024-01-08 07:32:22 +0530

    front page font added

---
+++ app/components/font/FontLoader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FontLoader = () => {
+  return (
+    <div>
+      <link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet" />
+
+      <svg viewBox="0 0 1320 300">
+        <text x="50%" y="50%" dy=".35em" textAnchor="middle" fontFamily="Russo One" fontSize="48">
+          My Todo
+        </text>
+      </svg>
+    </div>
+  );
+};
+
+export default FontLoader;
+
--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import LoadingGif from "./components/LoadingGif";
+import FontLoader from './components/font/FontLoader'
+
+// const [css_value, setCss_value]=useState('');
 export default function Home() {
   const pathname = usePathname();
   return (
     <div className="App">
-      <div className="home-page-loading"></div>
+      <div className="home-page-loading"><FontLoader /></div>
     </div>
   );
 }
