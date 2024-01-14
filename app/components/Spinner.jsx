4c454d2b12a819e53383709980326d39fd3c6c33
ManavYagnik <yagnik.c@ahduni.edu.in>
2024-01-14 10:31:01 +0530

    some changes in spinner

--- app/components/Spinner.jsx
+++ app/components/Spinner.jsx
@@ -4,7 +4,7 @@ import loader from "./spinner.gif";
 
 const Spinner = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="spinner-loader">
       <Image src={loader} alt="loading.." />
     </div>
   );
